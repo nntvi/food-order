c4c333639489085b5708de48cc8a5643d53c9ec5
nntvi <vietnamblockchain.asia>
2025-04-22 22:40:03 +0700

    get list dishes server comp

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000',
+        pathname: '/**'
+      }
+    ]
+  }
+}
 
 export default nextConfig
